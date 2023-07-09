a74c0c7ecabc89804c8a143698dc47c6ef97297d
Пользователь <01txxt10@gmail.com>
2023-07-09 18:10:17 +0700

    Вносит правки, сокращает пути в import-ах

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import MainPage from '../../pages/main/main';
+import { MainPage } from '../../pages/main';
 
 function App(): JSX.Element {
   return (
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,4 +1,4 @@
-import PlaceCard from '../../components/place-card/place-card';
+import { PlaceCard } from '../../components/place-card';
 import { Settings } from '../../settings';
 
 type MainPageProps = {
